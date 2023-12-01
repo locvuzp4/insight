e3d202c31a8da45120956fc938424e6711f10c83
root <root@insight.asia-southeast1-b.c.intercall-398816.internal>
2023-12-01 08:56:43 +0000

    add check border

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,26 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\HandlePercentPdfToPusher;
 use App\Http\Services\ExportExcel;
 use App\Jobs\PdfToImages;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
 
 class HomeController extends Controller
 {
     public function uploadFile(Request $request)
     {
-        // return [
-        //     'image_path' => config('app.url') . Storage::url('public/pages/page_0.jpg'),
-        //     'page_number' => 5
-        // ];
-
         if ($request->hasFile('file')) {
             $file = $request->file('file');
-            $path = $file->store('public');
+            $path = $file->store('public/upload');
             // $fullPath = config('app.url'). Storage::url($path);
             PdfToImages::dispatch(storage_path('app/' . $path));
         }
@@ -36,7 +28,7 @@ class HomeController extends Controller
         $header = ['Page'];
         $body = [];
 
-        $countValue = [];
+        $countValue = []; // số đáp án mỗi câu
         $points = [];
         $titles = [];
 
@@ -56,22 +48,77 @@ class HomeController extends Controller
                 $n++;
             }
         }
-        // dd($points);
         $countValue[] = $n;
 
+        $datas = [];
+        $baseX = 0;
+        $baseY = 0;
+
+        $changeArr = [];
         for ($n = 0; $n < $pageNumber; $n++) {
+            $changeX = 0;
+            $changeY = 0;
             $imagePath = storage_path('app/public/pages/page_' . $n . '.jpg');
             $image = Image::make($imagePath);
             $image->greyscale();
 
+            if ($request->type) {
+                if ($n == 0) {
+                    $width = $image->width();
+                    $height = $image->height();
+                    $halfW = round($width / 2);
+                    $halfH = round($height / 2);
+                }
+
+                $arr = [];
+                for ($i = 0; $i < $halfH; $i++) {
+                    $data = $image->pickColor($halfW, $i);
+                    // $arr[] = [$data[0], $dataNext[0], $dataNext2[0]];
+                    if ($data[0] < 200) {
+                        $dataNext = $image->pickColor($halfW + 1, $i);
+                        if ($dataNext[0] < 200) {
+                            $dataPre = $image->pickColor($halfW - 1, $i);
+                            if ($dataPre[0] < 200) {
+                                if ($n == 0) {
+                                    $baseY = $i;
+                                } else {
+                                    $changeY = $i - $baseY;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                for ($i = 0; $i < $halfW; $i++) {
+                    $data = $image->pickColor($i, $halfH);
+                    if ($data[0] < 200) {
+                        $dataNext = $image->pickColor($i, $halfH + 1);
+                        if ($dataNext[0] < 200) {
+                            $dataPre = $image->pickColor($i, $halfH - 1);
+                            if ($dataPre[0] < 200) {
+                                if ($n == 0) {
+                                    $baseX = $i;
+                                } else {
+                                    $changeX = $i - $baseX;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                $datas[] = [$changeX, $changeY];
+            }
+
+            $changeArr[] = [$changeX, $changeY];
             $value = [];
             foreach ($points as $point) {
                 try {
                     $colors = [];
                     for ($x = -10; $x < 11; $x++) {
                         for ($y = -10; $y < 11; $y++) {
-                            $pointX = $point[0] + $x;
-                            $pointY = $point[1] + $y;
+                            $pointX = $point[0] + $changeX + $x;
+                            $pointY = $point[1] + $changeY + $y;
                             $pixel = $image->pickColor($pointX, $pointY);
                             $colors[] = $pixel[0];
                         }
@@ -82,11 +129,15 @@ class HomeController extends Controller
                 }
             }
 
+            // if ($n == 48) {
+            //     return $value;
+            // }
+
             $maxValue = max($value);
             $result = [];
             $vitriDefault = -1;
             foreach ($value as $index => $item) {
-                if ($item < $maxValue - 20 && $item != 0) {
+                if ($item < $maxValue - 30 && $item != 0) {
                     $vitri = $this->layViTri($countValue, $index);
                     if ($vitriDefault == $vitri || $vitriDefault == $vitri - 1) {
                         $vitriDefault = $vitri;
@@ -97,8 +148,6 @@ class HomeController extends Controller
                     }
                     if (isset($result[$vitri])) {
                         $result[$vitri] = $result[$vitri] . ', ' . $titles[$index];
-                        // $value = str_replace('\n', nl2br("\n", true), $value);
-                        // $result[$vitri] = str_replace('<br />', '', $value);
                     } else {
                         $result[$vitri] = $titles[$index];
                     }
@@ -107,10 +156,9 @@ class HomeController extends Controller
             array_unshift($result, $n + 1);
             $body[] = $result;
         }
-        // dd($body);
+        // return $changeArr;
 
         return (new ExportExcel($header, $body))->export('Report.xlsx');
-        // return $body;
     }
 
     private function layViTri($countValue, $index)
